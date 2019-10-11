f865f3f914dc7a4f8efddfe966db1d3a85cd1252
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-11 05:33:37 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,8 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts["Freddy Mercury"].each {|attributes,value}
-    if attributes==favorite_ice_cream_flavors
+    if attributes==:favorite_ice_cream_flavors
+      
       
   
 
